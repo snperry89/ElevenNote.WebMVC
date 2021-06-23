dd1364a2850abf97d64c2836db32d5b9ff38a1b6
Sam Perry <snperry89@gmail.com>
2021-06-23 15:59:28 -0400

    Added ValidationMessages for NoteCreate

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,5 +30,43 @@ namespace ElevenNote.WebMVC.Controllers
             }
             return View(model);
         }
+
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Below method makes sure modelstate is valid, grabs the current userId, calls on CreateNote, and returns the user back to the index view
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            // Used Edit-Refactor-Extract_Method to create helper method that does the same as lines 36-37
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                /*ViewBag.SaveResult*/ 
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Note could not be created.");
+
+            return View(model);
+        }
+
+            private NoteService CreateNoteService()
+            {
+                var userId = Guid.Parse(User.Identity.GetUserId());
+                var service = new NoteService(userId);
+                return service;
+            }
+        }
     }
-}
